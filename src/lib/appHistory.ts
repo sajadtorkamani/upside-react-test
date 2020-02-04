ef6e65ebd5f94ef372a76d60b9fb409e200ffba7
Sajad Torkamani <sajadtorkamani1@gmail.com>
2020-02-04 04:42:54 +0000

    Scroll to top on route change

--- src/lib/appHistory.ts
+++ src/lib/appHistory.ts
@@ -2,4 +2,9 @@ import { createBrowserHistory } from 'history';
 
 const appHistory = createBrowserHistory();
 
+// Scroll to top on route change.
+appHistory.listen(() => {
+  window.scrollTo(0, 0);
+});
+
 export default appHistory;
